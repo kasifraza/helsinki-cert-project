b3ce5d65b9787e044a1eb6143c2282e005c9acc4
Kaif Raza <kasifraza910@gmail.com>
2026-06-29 21:22:53 +0530

    added latest migration

---
+++ drizzle/0001_wild_miracleman.sql
@@ -0,0 +1,11 @@
+CREATE TABLE "reading_list" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"user_id" integer NOT NULL,
+	"blog_id" integer NOT NULL,
+	"read" boolean DEFAULT false NOT NULL
+);
+--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "password_hash" text DEFAULT '' NOT NULL;--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "token" text;--> statement-breakpoint
+ALTER TABLE "reading_list" ADD CONSTRAINT "reading_list_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "reading_list" ADD CONSTRAINT "reading_list_blog_id_blogs_id_fk" FOREIGN KEY ("blog_id") REFERENCES "public"."blogs"("id") ON DELETE no action ON UPDATE no action;
